79d3b0f757a97d6ee8296798a01c8f4509082020
Rishabh Pandey <rishabh.pandey995@gmail.com>
2025-12-04 22:55:41 +0530

    Update build files

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,14 @@
 echo "Building project..."
 yarn build
 
-# Add and commit the dist folder to main branch
+# Remove old docs folder if it exists
+rm -rf docs
+
+# Move dist to docs
+echo "Moving build to docs folder..."
+mv dist docs
+
+# Add and commit the docs folder to main branch
 echo "Committing build files..."
 git add .
 git commit -m "Update build files"
@@ -14,3 +21,4 @@ echo "Pushing to GitHub..."
 git push origin main
 
 echo "Deployment complete! Access your site at: https://rishpandey.github.io/my-habitica/"
+echo "Make sure to configure GitHub Pages to serve from the /docs folder on main branch."
